a97fbb6f3a5bb7d4ee94abeef3209ba7c4aaaf1e
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-24 21:38:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length >= 1 
     return array.join
     elsif array.length == 2 
-     array = array.join(" ")
+     array = array.join(" and ")
      return array
   
 end
