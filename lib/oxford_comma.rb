e4c59f4b8ec88cb3effb7a0fdfeb692d414ae1c0
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-24 21:41:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,14 +9,3 @@ def oxford_comma(array)
 end
 end
 
-
-def oxford_comma(array)
-  if array.length == 1 
-    return array.join
-    elsif array.length == 2 
-      return array.join(" and ")
-    else array.length >= 3
-     array[-1].insert(0, "and ")
-     array.join(", ")
-  end
-end
