096f2768a4e367d1a350de6d2cba81e31e224ea2
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-24 21:39:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,17 @@
 def oxford_comma(array)
+  if array.length >= 1 
+    return array.join
+    elsif array.length == 2 
+     return array.join(" and ")
+   else array.length >= 3 
+     return.array.join(" ,")
+   end
+end
+end
+
+
+def oxford_comma(array)
   if array.length == 1 
     return array.join
     elsif array.length == 2 
       return array.join(" and ")
-end
-end
