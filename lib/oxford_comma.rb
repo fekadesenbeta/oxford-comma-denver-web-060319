6fb21fc4ddb59e058802a63a3745230545681215
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-24 21:41:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -15,3 +15,8 @@ def oxford_comma(array)
     return array.join
     elsif array.length == 2 
       return array.join(" and ")
+    else array.length >= 3
+     array[-1].insert(0, "and ")
+     array.join(", ")
+  end
+end
